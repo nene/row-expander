3d9653699f6d64117163965b25a4cc730d24bdc7
Rene Saarsoo <nene@triin.net>
2010-03-25 11:59:49 +0200

    Added Reload button to example to demonstrate bug.
    When enableCaching:true then on reloading store expanded items
    will become collapsed but the expand button will still remain
    as [-] not as [+].

--- example.js
+++ example.js
@@ -8,36 +8,36 @@
 Ext.onReady(function(){
 
     var dummyData = [
-        ['3m Co',71.72,0.02,0.03,'9/1 12:00am', 'Manufacturing'],
-        ['Alcoa Inc',29.01,0.42,1.47,'9/1 12:00am', 'Manufacturing'],
-        ['Altria Group Inc',83.81,0.28,0.34,'9/1 12:00am', 'Manufacturing'],
-        ['American Express Company',52.55,0.01,0.02,'9/1 12:00am', 'Finance'],
-        ['American International Group, Inc.',64.13,0.31,0.49,'9/1 12:00am', 'Services'],
-        ['AT&T Inc.',31.61,-0.48,-1.54,'9/1 12:00am', 'Services'],
-        ['Boeing Co.',75.43,0.53,0.71,'9/1 12:00am', 'Manufacturing'],
-        ['Caterpillar Inc.',67.27,0.92,1.39,'9/1 12:00am', 'Services'],
-        ['Citigroup, Inc.',49.37,0.02,0.04,'9/1 12:00am', 'Finance'],
-        ['E.I. du Pont de Nemours and Company',40.48,0.51,1.28,'9/1 12:00am', 'Manufacturing'],
-        ['Exxon Mobil Corp',68.1,-0.43,-0.64,'9/1 12:00am', 'Manufacturing'],
-        ['General Electric Company',34.14,-0.08,-0.23,'9/1 12:00am', 'Manufacturing'],
-        ['General Motors Corporation',30.27,1.09,3.74,'9/1 12:00am', 'Automotive'],
-        ['Hewlett-Packard Co.',36.53,-0.03,-0.08,'9/1 12:00am', 'Computer'],
-        ['Honeywell Intl Inc',38.77,0.05,0.13,'9/1 12:00am', 'Manufacturing'],
-        ['Intel Corporation',19.88,0.31,1.58,'9/1 12:00am', 'Computer'],
-        ['International Business Machines',81.41,0.44,0.54,'9/1 12:00am', 'Computer'],
-        ['Johnson & Johnson',64.72,0.06,0.09,'9/1 12:00am', 'Medical'],
-        ['JP Morgan & Chase & Co',45.73,0.07,0.15,'9/1 12:00am', 'Finance'],
-        ['McDonald\'s Corporation',36.76,0.86,2.40,'9/1 12:00am', 'Food'],
-        ['Merck & Co., Inc.',40.96,0.41,1.01,'9/1 12:00am', 'Medical'],
-        ['Microsoft Corporation',25.84,0.14,0.54,'9/1 12:00am', 'Computer'],
-        ['Pfizer Inc',27.96,0.4,1.45,'9/1 12:00am', 'Services', 'Medical'],
-        ['The Coca-Cola Company',45.07,0.26,0.58,'9/1 12:00am', 'Food'],
-        ['The Home Depot, Inc.',34.64,0.35,1.02,'9/1 12:00am', 'Retail'],
-        ['The Procter & Gamble Company',61.91,0.01,0.02,'9/1 12:00am', 'Manufacturing'],
-        ['United Technologies Corporation',63.26,0.55,0.88,'9/1 12:00am', 'Computer'],
-        ['Verizon Communications',35.57,0.39,1.11,'9/1 12:00am', 'Services'],
-        ['Wal-Mart Stores, Inc.',45.45,0.73,1.63,'9/1 12:00am', 'Retail'],
-        ['Walt Disney Company (The) (Holding Company)',29.89,0.24,0.81,'9/1 12:00am', 'Services']
+        [1, '3m Co',71.72,0.02,0.03,'9/1 12:00am', 'Manufacturing'],
+        [2, 'Alcoa Inc',29.01,0.42,1.47,'9/1 12:00am', 'Manufacturing'],
+        [3, 'Altria Group Inc',83.81,0.28,0.34,'9/1 12:00am', 'Manufacturing'],
+        [4, 'American Express Company',52.55,0.01,0.02,'9/1 12:00am', 'Finance'],
+        [5, 'American International Group, Inc.',64.13,0.31,0.49,'9/1 12:00am', 'Services'],
+        [6, 'AT&T Inc.',31.61,-0.48,-1.54,'9/1 12:00am', 'Services'],
+        [7, 'Boeing Co.',75.43,0.53,0.71,'9/1 12:00am', 'Manufacturing'],
+        [8, 'Caterpillar Inc.',67.27,0.92,1.39,'9/1 12:00am', 'Services'],
+        [9, 'Citigroup, Inc.',49.37,0.02,0.04,'9/1 12:00am', 'Finance'],
+        [10, 'E.I. du Pont de Nemours and Company',40.48,0.51,1.28,'9/1 12:00am', 'Manufacturing'],
+        [11, 'Exxon Mobil Corp',68.1,-0.43,-0.64,'9/1 12:00am', 'Manufacturing'],
+        [12, 'General Electric Company',34.14,-0.08,-0.23,'9/1 12:00am', 'Manufacturing'],
+        [13, 'General Motors Corporation',30.27,1.09,3.74,'9/1 12:00am', 'Automotive'],
+        [14, 'Hewlett-Packard Co.',36.53,-0.03,-0.08,'9/1 12:00am', 'Computer'],
+        [15, 'Honeywell Intl Inc',38.77,0.05,0.13,'9/1 12:00am', 'Manufacturing'],
+        [16, 'Intel Corporation',19.88,0.31,1.58,'9/1 12:00am', 'Computer'],
+        [17, 'International Business Machines',81.41,0.44,0.54,'9/1 12:00am', 'Computer'],
+        [18, 'Johnson & Johnson',64.72,0.06,0.09,'9/1 12:00am', 'Medical'],
+        [19, 'JP Morgan & Chase & Co',45.73,0.07,0.15,'9/1 12:00am', 'Finance'],
+        [20, 'McDonald\'s Corporation',36.76,0.86,2.40,'9/1 12:00am', 'Food'],
+        [21, 'Merck & Co., Inc.',40.96,0.41,1.01,'9/1 12:00am', 'Medical'],
+        [22, 'Microsoft Corporation',25.84,0.14,0.54,'9/1 12:00am', 'Computer'],
+        [23, 'Pfizer Inc',27.96,0.4,1.45,'9/1 12:00am', 'Services', 'Medical'],
+        [24, 'The Coca-Cola Company',45.07,0.26,0.58,'9/1 12:00am', 'Food'],
+        [25, 'The Home Depot, Inc.',34.64,0.35,1.02,'9/1 12:00am', 'Retail'],
+        [26, 'The Procter & Gamble Company',61.91,0.01,0.02,'9/1 12:00am', 'Manufacturing'],
+        [27, 'United Technologies Corporation',63.26,0.55,0.88,'9/1 12:00am', 'Computer'],
+        [28, 'Verizon Communications',35.57,0.39,1.11,'9/1 12:00am', 'Services'],
+        [29, 'Wal-Mart Stores, Inc.',45.45,0.73,1.63,'9/1 12:00am', 'Retail'],
+        [30, 'Walt Disney Company (The) (Holding Company)',29.89,0.24,0.81,'9/1 12:00am', 'Services']
     ];
 
     // add in some dummy descriptions
@@ -54,7 +54,8 @@ Ext.onReady(function(){
         );
     }
 
-    var reader = new Ext.data.ArrayReader({}, [
+    var reader = new Ext.data.ArrayReader({idIndex: 0}, [
+       {name: 'id'},
        {name: 'company'},
        {name: 'price', type: 'float'},
        {name: 'change', type: 'float'},
@@ -68,7 +69,8 @@ Ext.onReady(function(){
         tpl: new Ext.Template(
             '<p><b>Company:</b> {company}</p><br>',
             '<p><b>Summary:</b> {desc}</p>'
-        )
+        ),
+        enableCaching: false
     });
 
     var grid = new Ext.grid.GridPanel({
@@ -93,6 +95,12 @@ Ext.onReady(function(){
         viewConfig: {
             forceFit: true
         },
+        tbar: [{
+            text: "Reload",
+            handler: function() {
+                grid.getStore().loadData(dummyData);
+            }
+        }],
         width: 600,
         height: 300,
         plugins: expander,
